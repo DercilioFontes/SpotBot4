a75c84ba9dc2daed265d5260044b274094802fab
Prerana Shrestha <prerana_sh@gmail.com>
2018-03-12 15:53:34 -0700

    Implemented map on home page

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { StyleSheet, Button, Text, View, Image } from 'react-native'
-import { StackNavigator } from 'react-navigation'
+import {MapView, Notifications } from 'expo';
+import { StackNavigator, TabBarBottom } from 'react-navigation';
+import MapHome from './component/MapHome.js'
+
 
 class LogoTitle extends React.Component {
   render() {
@@ -13,43 +16,131 @@ class LogoTitle extends React.Component {
   }
 }
 
-// class LogoEVCar extends React.Component {
-//   render() {
-//     return (
-//       <Image
-//         source={require('./src/evubc-logo.png')}
-//         style={{ width: 90, height: 78, marginTop: 400 }}
-//       />
-//     );
-//   }
-// }
-
 class HomeScreen extends React.Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+            isFirstLoad: true,
+            annotations: [],
+            mapRegion: {
+              latitude: 49.269966,
+              longitude: -123.251043,
+              latitudeDelta: 0.004,
+              longitudeDelta: 0.04,
+              title: "UBC parking slot"
+            },
+            parking_areas: [{
+                title: 'Marker1',
+                parkingAreaStatus: 'full',
+                description: 'Description about spot1',
+                coordinates: {
+                  latitude: 49.269966,
+                  longitude: -123.251043
+                },
+                slots: [{
+                  label: 'spot1',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                  },
+                  {
+                  label: 'spot2',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                  },
+                  {
+                  label: 'spot3',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                }]
+              },
+              {
+                title: 'Marker2',
+                description: 'Description about spot2',
+                parkingAreaStatus: 'reserved',
+                coordinates: {
+                  latitude: 49.264554,
+                  longitude: -123.255521
+                },
+                slots: [{
+                  label: 'spot1',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                  },
+                  {
+                  label: 'spot2',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                  },
+                  {
+                  label: 'spot3',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                }]
+              },
+              {
+                title: 'Marker3',
+                description: 'Description about spot3',
+                parkingAreaStatus: 'reserved',
+                coordinates: {
+                  latitude: 49.261811,
+                  longitude: -123.243056
+                },
+                slots: [{
+                  label: 'spot1',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                  },
+                  {
+                  label: 'spot2',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                  },
+                  {
+                  label: 'spot3',
+                  occupied: true,
+                  accessible: false,
+                  spot_information: "this is spot 1"
+                }]
+              },
+              ]
+            };
+        }
+
   static navigationOptions = {
     headerTitle: 'SpotBot',
   }
 
+
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Map Screen</Text>
-      <Button
-        title="Login / Sign up"
-        onPress={() => this.props.navigation.navigate('Details', {
-          spotId: 4,
-          spotLabel: 'UBC01',
-        })}
-      />
-      {/* <LogoEVCar/> */}
-    </View>
+      <MapHome parking_areas={this.state.parking_areas} mapRegion={this.state.mapRegion}>
+        <Button
+          title="Login / Sign up"
+          onPress={() => this.props.navigation.navigate('Details', {
+            spotId: 4,
+            spotLabel: 'UBC01',
+          })}
+        />
+      </MapHome>
     )
   }
 }
 
+
+
 class DetailsScreen extends React.Component {
   static navigationOptions = ({ navigation, navigationOptions }) => {
     const { params } = navigation.state;
-    
+
     return {
       title: params ? params.spotLabel : 'A Nested Details Screen',
       headerStyle: {
@@ -116,3 +207,42 @@ export default class App extends React.Component {
     return <RootStack />;
   }
 }
+
+
+const styles = StyleSheet.create({
+  full: {
+    borderWidth:3,
+    borderColor: 'white',
+    backgroundColor: 'red',
+    borderRadius: 15,
+    width:30,
+    height:30,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  reserved:{
+    borderWidth:3,
+    borderColor: 'white',
+    backgroundColor: 'green',
+    borderRadius: 15,
+    width:30,
+    height:30,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  radius: {
+    backgroundColor: '#fff',
+  },
+  marker: {
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+  },
+});

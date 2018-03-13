bd75b4d2b3328d489a366431bc8429880713d441
DercilioFontes <dercilioafontes@gmail.com>
2018-03-13 13:15:26 -0700

    Implemented saving form data in the DB.

--- App.js
+++ App.js
@@ -1,9 +1,49 @@
 import React from 'react'
-import { StyleSheet, Button, Text, View, Image } from 'react-native'
+import { StyleSheet, Button, Text, View, Image, TouchableHighlight, ActivityIndicator, AlertIOS } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 import { Ionicons, Feather } from '@expo/vector-icons'
 
+// to use in the forms
+import t from 'tcomb-form-native'
+const Form = t.form.Form
 
+// Config of the form data and options
+const User = t.struct({
+  name: t.String,
+  licensePlate: t.String,
+  email: t.String,
+  password: t.String,
+  passwordConfirmation: t.String
+})
+
+// Options of the form data
+const options = {
+  fields: {
+    name: {
+
+    },
+    licensePlate: {
+
+    },
+    email: {
+      keyboardType: 'email-address'
+    },
+    password: { 
+      secureTextEntry: true
+    },
+    passwordConfirmation: {
+      secureTextEntry: true
+      // hasError: true,
+      // error: (value) => {
+      //   if(value.password !== value.passwordConfirmation) {
+      //     return "Passowords doesn't macth!"
+      //   }
+      // }
+    }
+  }
+}
+
+// Main Screen
 class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     const params = navigation.state.params || {}
@@ -37,6 +77,7 @@ class HomeScreen extends React.Component {
   }
 }
 
+// Modal of the headers button
 class ModalScreen extends React.Component {
   render() {
     return (
@@ -66,11 +107,19 @@ class ModalScreen extends React.Component {
 }
 
 class SignUpScreen extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      showProgress: false
+    }
+  }
+
   static navigationOptions = ({ navigation, navigationOptions }) => {
     const { params } = navigation.state;
     
     return {
-      title: params ? params.spotLabel : 'A Nested Details Screen',
+      title: 'Sign Up',
       headerStyle: {
         backgroundColor: navigationOptions.headerTintColor,
       },
@@ -78,28 +127,191 @@ class SignUpScreen extends React.Component {
     }
   }
 
+  // async _onValueChange(item, selectedValue) {
+  //   try {
+  //     await AsyncStorage.setItem(item, selectedValue);
+  //   } catch (error) {
+  //     console.log('AsyncStorage error: ' + error.message);
+  //   }
+  // }
+
+  _userSignup() {
+    let value = this.refs.form.getValue();
+
+
+    // REMOVE THIS!!!!!!
+    console.log(value)
+
+    // REMOVE THIS!!!!!!!!!!
+    console.log(
+    JSON.stringify({
+      name: value.name, 
+      license_plate: value.licensePlate, 
+      email: value.email,
+      password: value.password,
+      password_confirmation: value.passwordConfirmation, 
+    }))
+    if (value) { // if validation fails, value will be null
+
+      this.setState({showProgress: true})
+
+      fetch("https://spot-bot-server.herokuapp.com/users", {
+        method: "POST", 
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: value.name, 
+          license_plate: value.licensePlate, 
+          email: value.email,
+          password: value.password,
+          password_confirmation: value.passwordConfirmation, 
+        })
+      })
+      .then((response) => {
+      if(response.status >= 200 && response.status < 300) {
+        return response
+      }
+      throw {
+        badCredentials: response.status == 401,
+        unknownError: response.status != 401
+      }
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((responseData) => {
+      console.log(responseData)
+      this.setState({ success: true })
+      // this._onValueChange(STORAGE_KEY, responseData.id_token),
+      // AlertIOS
+    })
+    .catch((err) => {
+      this.setState(err)
+      console.log(err)
+    })
+    .finally(() => {
+      this.setState({showProgress: false})
+    })
+    }
+  }
+
+  
+  // call getValue() to get the values of the form
+  // onPress() {
+  //   var value = this.refs.form.getValue();
+  //   if (value) { // if validation fails, value will be null
+
+  //     // Active ActivityIndicator 
+  //     this.setState({showProgress: true})
+
+  //     console.log(
+  //     JSON.stringify({
+  //       user: {
+  //         name: value.name,
+  //         license_plate: value.licensePlate,
+  //         email: value.email,
+  //         password: value.password,
+  //         password_confirmation: value.passwordConfirmation
+  //       }
+  //     }))
+
+  //     // fetch('https://spot-bot-server.herokuapp.com/users', {
+  //     //   method: 'Post',
+  //     //   headers: {
+  //     //     'Accept': 'application/json',
+  //     //     'Content-Type': 'aplication/json',
+  //     //   },
+  //     //   body: JSON.stringify({
+  //     //     user: {
+  //     //       name: value.name,
+  //     //       license_plate: value.licensePlate,
+  //     //       email: value.email,
+  //     //       password: value.password,
+  //     //       password_confirmation: value.passwordConfirmation
+  //     //     }
+  //     //   })
+  //     // })
+  //     // .then((response) => {
+  //     //   if(response.status >= 200 && response.status < 300) {
+  //     //     return response
+  //     //   }
+  //     //   throw {
+  //     //     badCredentials: response.status == 401,
+  //     //     unknownError: response.status != 401
+  //     //   }
+  //     // })
+  //     // .then((response) => {
+  //     //   return response.json()
+  //     // })
+  //     // .then((responseData) => {
+  //     //   console.log(responseData)
+  //     // })
+  //     // .catch((err) => {
+  //     //   this.setState(err)
+  //     //   console.log(err)
+  //     // })
+  //     // .finally(() => {
+  //     //   this.setState({showProgress: false})
+  //     // })
+  //   }
+  // }
+
   render() {
-
     const { params } = this.props.navigation.state;
-    const spotId = params ? params.spotId : null;
-    const spotLabel = params ? params.spotLabel : null;
 
+
+    const styles = StyleSheet.create({
+      container: {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+      },
+      title: {
+        fontSize: 30,
+        alignSelf: 'center',
+        marginBottom: 30
+      },
+      buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+      },
+      button: {
+        height: 36,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+      },
+      loader: {
+        marginTop: 20
+      }
+    });
+    
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ marginBottom: 10 }}>Login / Sign up</Text>
-        <Text>Spot picked: {spotLabel}</Text>
-        {/* <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.navigate('Details')}
-        /> */}
-        {/* <Button
-          title="Update the title"
-          onPress={() => this.props.navigation.setParams({spotLabel: 'Updated!'})}
-        /> */}
+        <Form
+          ref="form"
+          type={User}
+          options={options}
+        />
+         <TouchableHighlight style={styles.button} onPress={this._userSignup.bind(this)} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableHighlight>
         <Button
           title="Home"
           onPress={() => this.props.navigation.goBack()}
         />
+        <ActivityIndicator
+        animating={this.state.showProgress}
+        size='large'
+        style={styles.loader}/>
       </View>
     );
   }

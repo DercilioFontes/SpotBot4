f44a8b27a1d8e9f8aab7c9649fa90fb839ec3056
DercilioFontes <dercilioafontes@gmail.com>
2018-03-18 10:55:26 -0700

    Some changes in modal screen, but parcial implemented.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Button, Text, View, Image, TouchableHighlight, ActivityIndicator, AlertIOS, AsyncStorage, TouchableOpacity } from 'react-native'
+import { StyleSheet, Button, Text, View, Image, TouchableHighlight, ActivityIndicator, AlertIOS, AsyncStorage, TouchableOpacity, ImageBackground } from 'react-native'
 import { MapView, Notifications } from 'expo'
 import { StackNavigator } from 'react-navigation'
 import { Ionicons, Feather, MaterialIcons, FontAwesome } from '@expo/vector-icons'
@@ -231,22 +231,27 @@ class HomeScreen extends React.Component {
 
 class ModalScreen extends React.Component {
   render() {
+    const pic = require('./assets/vancouver.jpg')
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>SpotBot App</Text>
-        <Button
+      <ImageBackground source= {pic} style={{ width: 250, opacity: .7,  flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text style={styles.main_header }>SpotBot</Text>
+        <Text
+          style={styles.buttonOutline}
           title="Login"
           onPress={() => this.props.navigation.navigate('Login')}
-        />
-        <Button
+        >LOGIN</Text>
+        <Text
+          style={styles.buttonFilledIn}
           title="Sign up"
           onPress={() => this.props.navigation.navigate('SignUp')}
-        />
+        >SIGNUP</Text>
         <Button
           onPress={() => this.props.navigation.goBack()}
           title="Dismiss"
         />
-      </View>
+      </ImageBackground>
     );
   }
 }
@@ -327,10 +332,30 @@ const MainStack = StackNavigator(
       headerTintColor: '#fff',
       headerTitleStyle: {
         fontWeight: 'bold',
-      },
-    }
+      }
+    },
+    // cardStyle: {
+    //   backgroundColor: '#000000',
+    // }
   }
 );
+
+// function forVertical(props) {
+//   const { layout, position, scene } = props;
+
+//   const index = scene.index;
+//   const height = layout.initHeight;
+
+//   const translateX = 0;
+//   const translateY = position.interpolate({
+//     inputRange: ([index - 1, index, index + 1]: Array<number>),
+//     outputRange: ([height, 0, 0]: Array<number>)
+//   });
+
+//   return {
+//     transform: [{ translateX }, { translateY }]
+//   };
+// }
 
 const RootStack = StackNavigator(
   {
@@ -344,8 +369,46 @@ const RootStack = StackNavigator(
   {
     mode: 'modal',
     headerMode: 'none',
+    // transitionConfig: () => ({ screenInterpolator: forVertical })
   }
 );
+
+const styles = StyleSheet.create({
+  buttonOutline: {
+    borderWidth:2,
+    borderColor: 'white',
+    color: 'white',
+    padding: 10,
+    paddingHorizontal: 30,
+    fontSize: 20,
+    // backgroundColor: 'red',
+    borderStyle: 'solid',
+    // borderRadius: 10,
+    width:200,
+    textAlign: 'center',
+    margin: 15
+  },
+  buttonFilledIn: {
+    borderWidth:2,
+    borderColor: 'white',
+    color: 'grey',
+    padding: 10,
+    paddingHorizontal: 30,
+    fontSize: 20,
+    width:200,
+    textAlign: 'center',
+    backgroundColor: 'white',
+    borderStyle: 'solid',
+    borderRadius: 10,
+    margin: 15
+  },
+  main_header: {
+    fontSize: 50,
+    color: 'black',
+    marginBottom: 200
+
+  }
+});
 
 export default class App extends React.Component {
   render() {

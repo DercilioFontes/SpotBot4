0e3e6e55061d8ba1e751fa60f3e08f8e8d97547f
DercilioFontes <dercilioafontes@gmail.com>
2018-03-18 18:39:26 -0700

    Partial implementation of modal in left header button.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Button, Text, View, Image, TouchableHighlight, ActivityIndicator, AlertIOS, AsyncStorage, TouchableOpacity, ImageBackground, Modal} from 'react-native'
+import { StyleSheet, Button, Text, View, Image, TouchableHighlight, ActivityIndicator, AlertIOS, AsyncStorage, TouchableOpacity, ImageBackground } from 'react-native'
 import { MapView, Notifications } from 'expo'
 import { StackNavigator, DrawerNavigator } from 'react-navigation'
 import { Ionicons, Feather, MaterialIcons, FontAwesome } from '@expo/vector-icons'
@@ -15,7 +15,6 @@ import { typography } from 'react-native-material-design-styles';
 const typographyStyle = StyleSheet.create(typography);
 import parkingAreasDB from './db/database'
 
-
 // Main Screen
 class HomeScreen extends React.Component {
 
@@ -23,61 +22,58 @@ class HomeScreen extends React.Component {
     super(props);
     this.state = {
       searchSpotStatus: false,
-        showSlotsDetails: false,
-        annotations: [],
-        mapRegion: {
-          latitude: 49.26,
-          longitude: -123.25,
-          latitudeDelta: 0.004,
-          longitudeDelta: 0.024,
-          title: "UBC parking slot"
+      showSlotsDetails: false,
+      isModalVisible: false,
+      annotations: [],
+      mapRegion: {
+        latitude: 49.26,
+        longitude: -123.25,
+        latitudeDelta: 0.004,
+        longitudeDelta: 0.024,
+        title: "UBC parking slot"
+      },
+      users: {
+        user_id: 2,
+        name: 'empty'
+      },
+      currentArea: {
+        title: 'Marker1',
+        parkingAreaStatus: 'full',
+        description: 'Description about spot1',
+        coordinates: {
+          latitude: 49.269966,
+          longitude: -123.251043,
         },
-        users: {
-          user_id: 2,
-          name: 'empty'
-        },
-        currentArea: {
-            title: 'Marker1',
-            parkingAreaStatus: 'full',
-            description: 'Description about spot1',
-            coordinates: {
-              latitude: 49.269966,
-              longitude: -123.251043,
-            },
-            slots: [{
-              spot_id: 1,
-              label: 'spot1',
-              occupied: true,
-              accessible: false,
-              spot_information: "this is spot 1"
-              },
-              {
-              spot_id: 1,
-              label: 'spot2',
-              occupied: true,
-              accessible: false,
-              spot_information: "this is spot 1"
-              },
-              {
-              spot_id: 1,
-              label: 'spot3',
-              occupied: true,
-              accessible: true,
-              spot_information: "this is spot 1"
-            }]
+        slots: [{
+          spot_id: 1,
+          label: 'spot1',
+          occupied: true,
+          accessible: false,
+          spot_information: "this is spot 1"
           },
-        parking_areas: parkingAreasDB
-        };
-    }
-
-
-
-
+          {
+          spot_id: 1,
+          label: 'spot2',
+          occupied: true,
+          accessible: false,
+          spot_information: "this is spot 1"
+          },
+          {
+          spot_id: 1,
+          label: 'spot3',
+          occupied: true,
+          accessible: true,
+          spot_information: "this is spot 1"
+        }]
+      },
+      parking_areas: parkingAreasDB
+    };
+  }
 
   static navigationOptions = ({ navigation }) => {
+    
     function searchSpot() {
       alert('uou');
-
     }
     const params = navigation.state.params || {}
 
@@ -87,17 +83,20 @@ class HomeScreen extends React.Component {
       },
       headerTitle: `SPOTBOT`,
       headerRight: (
-        <Button
-          onPress={searchSpot}
-          title='Filter'
-          color="#fff"
-        /> ),
+        <View>
+          <Button
+            onPress={searchSpot}
+            title='Filter'
+            color="#fff"
+          />
+        </View> ),
       headerLeft: (
         <Button
           onPress={() => navigation.navigate('MyModal')}
           title={'\u2630'}
           color="#FFF"
-        /> )
+        /> 
+      )
     }
   }
 
@@ -141,27 +140,22 @@ class HomeScreen extends React.Component {
     }
   }
 
-
-
-
   transformRaw(pa) {
-      console.log('pa inside transformRaw', pa)
-      return pa.parking_areas.map(raw_area => {
-        return {
-              parking_area_id: raw_area.parking_area.id,
-              title: raw_area.parking_area.parking_area_name,
-              description: raw_area.parking_area.parking_area_name,
-              parkingAreaStatus:'reserved',
-              coordinates: {
-                latitude: raw_area.parking_area.latitude,
-                longitude: raw_area.parking_area.longitude,
-              },
-              slots: raw_area.spots
-
-        }
+    console.log('pa inside transformRaw', pa)
+    return pa.parking_areas.map(raw_area => {
+      return {
+        parking_area_id: raw_area.parking_area.id,
+        title: raw_area.parking_area.parking_area_name,
+        description: raw_area.parking_area.parking_area_name,
+        parkingAreaStatus:'reserved',
+        coordinates: {
+          latitude: raw_area.parking_area.latitude,
+          longitude: raw_area.parking_area.longitude,
+        },
+        slots: raw_area.spots
+      }
     })
   }
-
 
   availability(parkingAreas) {
     console.log("availPAs", parkingAreas);
@@ -174,51 +168,41 @@ class HomeScreen extends React.Component {
           parkingArea.parkingAreaStatus = 'full',
           parkingArea.description = `Total slots: ${totalSlot} Status:Full`
       }
-
       return parkingArea;
     });
   }
 
 
+  componentDidMount() {
+    const that = this;
+    fetch('http://127.0.0.1:3000/')
+      .then(res => res.json())
+      .then(this.transformRaw)
+      .then(this.availability)
+      .then(newParkingStatus => {
+        that.setState({parking_areas: newParkingStatus})
+    })
+  }
 
+  filterAccessibility() {
+    var newParking = this.state.currentArea;
+    const accessibles = this.state.currentArea.slots.filter(slot => slot.accessible === true && slot.availability === true);
+    console.log(accessibles);
+    this.setState({
+      currentArea:{slots: accessibles},
+      showSlotsDetails: true
+    })
+  }
 
+  homePage(newParkingArea) {
+    const transformRawParking = this.transformRaw(newParkingArea);
+    this.setState({
+      showSlotsDetails: false,
+      parking_areas: this.availability(transformRawParking)
+    })
+    console.log("parking area",this.state.parking_areas);
 
-    componentDidMount() {
-
-
-
-      const that = this;
-      fetch('http://127.0.0.1:3000/')
-        .then(res => res.json())
-        .then(this.transformRaw)
-        .then(this.availability)
-        .then(newParkingStatus =>
-          {
-
-            that.setState({parking_areas: newParkingStatus})
-        })
-    }
-
-    filterAccessibility() {
-      var newParking = this.state.currentArea;
-      const accessibles = this.state.currentArea.slots.filter(slot => slot.accessible === true && slot.availability === true);
-      console.log(accessibles);
-      this.setState({
-        currentArea:{slots: accessibles},
-        showSlotsDetails: true
-      })
-    }
-
-    homePage(newParkingArea) {
-      const transformRawParking = this.transformRaw(newParkingArea);
-      this.setState({
-        showSlotsDetails: false,
-        parking_areas: this.availability(transformRawParking)
-      })
-     console.log("parking area",this.state.parking_areas);
-
-    }
-
+  }
 
   render() {
     return (
@@ -240,43 +224,44 @@ class HomeScreen extends React.Component {
               <SlotsScreen homePage={this.homePage.bind(this)} key={1} user_id={this.state.user_id} slots={this.state.currentArea.slots} />
             </View>
         }
-
       </View>
     )
   }
 }
-
 
 class ModalScreen extends React.Component {
   state = {
     visibleModal: null,
   }
 
-
-
-
   render() {
     const pic = require('./assets/vancouver.jpg')
 
     return (
-      <ImageBackground transparent = {true} source= {pic} style={{ opacity: .7,  flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, flexDirection: 'row'}}>
+        <ImageBackground source={pic} style={{ width: 300, opacity: .7,  flex: 2, alignItems: 'center', justifyContent: 'center' }}>
 
-        <Text style={styles.main_header }>SpotBot</Text>
-        <Text
-          style={styles.buttonOutline}
-          title="Login"
-          onPress={() => this.props.navigation.navigate('Login')}
-        >LOGIN</Text>
-        <Text
-          style={styles.buttonFilledIn}
-          title="Sign up"
-          onPress={() => this.props.navigation.navigate('SignUp')}
-        >SIGNUP</Text>
-        <Button
-          onPress={() => this.props.navigation.goBack()}
-          title="Dismiss"
-        />
-      </ImageBackground>
+          <Text style={styles.main_header }>SpotBot</Text>
+          <Text
+            style={styles.buttonOutline}
+            title="Login"
+            onPress={() => this.props.navigation.navigate('Login')}
+          >LOGIN</Text>
+          <Text
+            style={styles.buttonFilledIn}
+            title="Sign up"
+            onPress={() => this.props.navigation.navigate('SignUp')}
+          >SIGNUP</Text>
+          <Button
+            onPress={() => this.props.navigation.goBack()}
+            title="Dismiss"
+          />
+        </ImageBackground>
+        <View style={{flex: 1}}>
+          <HomeScreen >
+          </HomeScreen>
+        </View>
+      </View>
     );
   }
 }
@@ -303,8 +288,6 @@ class SignUpScreen extends React.Component {
   }
 
   render() {
-
-
     return ( <SignUpComponent navigation={this.props.navigation}/>)
   }
 }
@@ -317,7 +300,6 @@ class LoginScreen extends React.Component {
       navigationOptions: this.navigationOptions
     }
   }
-
 
   static navigationOptions = ({ navigation, navigationOptions }) => {
     const { params } = navigation.state;
@@ -332,7 +314,6 @@ class LoginScreen extends React.Component {
   }
 
   render() {
-
     return ( <LoginComponent navigation={this.props.navigation}/>)
   }
 }
@@ -412,7 +393,6 @@ const styles = StyleSheet.create({
     fontSize: 50,
     color: 'black',
     marginBottom: 200
-
   }
 });
 export default class App extends React.Component {
